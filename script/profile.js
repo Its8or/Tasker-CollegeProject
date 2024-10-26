e662e01c38fdb87699935cad12c4b950cce410f9
Lourivalrsn <vexlourival1234@gmail.com>
2024-10-26 15:19:17 -0300

    comentei nos códigos de html e no java script

--- script/profile.js
+++ script/profile.js
@@ -12,25 +12,33 @@ function loadUserData() {
 }
 
 function toggleForms() {
+    // Obtém os elementos dos contêineres de login e de cadastro pelo ID
     const loginContainer = document.getElementById('login-container');
     const signupContainer = document.getElementById('signup-container');
+// Verifica se o contêiner de login está oculto
     if (loginContainer.style.display === "none") {
+    // Exibe o contêiner de login e oculta o contêiner de cadastro
         loginContainer.style.display = "flex";
         signupContainer.style.display = "none";
     } else {
+    // Oculta o contêiner de login e exibe o contêiner de cadastro    
         loginContainer.style.display = "none";
         signupContainer.style.display = "flex";
     }
 }
 
 function saveUserData() {
+    // Obtém o valor dos campos nome, data de nascimento e imagem de perfil
     const name = document.getElementById('name').value;
     const birthdate = document.getElementById('birthdate').value;
     const profilePic = document.getElementById('profilePicInput').files[0];
 
+    // Se o usuário escolheu uma imagem de perfil, lê o arquivo
     if (profilePic) {
         const reader = new FileReader();
         reader.onload = function (event) {
+        
+        // Armazena a imagem em formato de base64 no localStorage e exibe-a    
             localStorage.setItem('userProfilePic', event.target.result);
             document.getElementById('profilePic').src = event.target.result;
         };
@@ -43,12 +51,15 @@ function saveUserData() {
 }
 
 function logout() {
+    // Remove os dados do usuário do localStorage (email, senha, nome, data de nascimento e imagem de perfil)
+    localStorage.removeItem('userEmail');
     localStorage.removeItem('userEmail');
     localStorage.removeItem('userPassword');
     localStorage.removeItem('userName');
     localStorage.removeItem('userBirthdate');
     localStorage.removeItem('userProfilePic');
+// Redireciona o usuário para a tela de login    
     window.location.href = "Tela-de-login.html";
 }
-
+// Carrega os dados do usuário ao carregar a página
 window.onload = loadUserData;
